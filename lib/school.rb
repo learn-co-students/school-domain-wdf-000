82ce09b3d3f28bb0fed9979415bcb326c33a89ad
blessedsoy <unmigong@gmail.com>
2016-06-23 22:49:27 -0400

    OO School Domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+class School
+
+
+
+def initialize (name)
+  @name = name
+  @roster = {}
+end
+
+def roster
+ @roster
+end
+
+
+def add_student(name, grade)
+
+  if @roster[grade] == nil 
+    @roster[grade] = []
+  end
+  
+  @roster[grade] << name
+end
+
+def grade(number)
+  roster[number]
+end
+
+def sort
+  # @roster = @roster.sort
+  @roster.each {|keys,values| values.sort!}
+
+  return roster
+end
+
+end
+
