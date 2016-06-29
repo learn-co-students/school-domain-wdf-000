2b795507ad9bad819a9020b8a13581c439063354
fidelscodes <fidelscodes@gmail.com>
2016-06-29 09:57:59 -0400

    Update with solution

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
+require "pry"
 # code here!
+
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {} # allows us to access the hash from anywhere inside the class
+  end
+
+  # if grade is already a key, add the student to that array
+  # else create the key and set it to empty array, then push student into array
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster.fetch(grade)
+  end
+
+  # TODO: #sort - sorts students alphabetically
+  def sort
+      @roster.each do |grade, students|
+        students.sort!
+      end
+  end
+end
+
+# binding.pry
