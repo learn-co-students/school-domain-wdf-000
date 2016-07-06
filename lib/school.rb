8add8ac7bea2af94ae745693cb288f4e585bb9f5
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-06 14:45:17 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_reader :roster, :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(student, year)
+    @roster[year] = [] if @roster[year] == nil
+    @roster[year] << student
+  end
+  def grade(year)
+    @roster[year]
+  end
+  def sort
+    @roster.keys.each do |year|
+      @roster[year] = @roster[year].sort
+    end
+    @roster
+  end
+end
