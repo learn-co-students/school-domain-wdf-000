90a3c164237aa8e0b937f4c11acc91999fb7ec3b
eutopian <eutopia@gmail.com>
2016-07-11 15:35:07 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :student, :grade, :roster
+  
+  def initialize(student)
+    @student = student
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+  if @roster[grade] == nil
+    @roster[grade] = []
+  end
+  @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student|
+    @roster[grade] = student.sort
+  end
+  end
+
+end
