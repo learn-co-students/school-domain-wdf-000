db0ca4c25db3c778de3e7060bc5084690207cbd2
satub <satu.barnhill@gmail.com>
2016-07-12 14:32:44 -0400

    Done with 2 sort methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ? @roster[grade] << student : @roster[grade] = [student]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  #The simple & pretty version
+  # def sort
+  #   @roster.each do |grade, students|
+  #      @roster[grade].sort!
+  #   end
+  # end
+
+  #The passing around objects-version just for the heck of it :D
+  def sort
+    @roster.keys.each_with_object({}) do |grade, sorted_students|
+      sorted_students[grade] = @roster[grade].sort
+    end
+  end
+end
