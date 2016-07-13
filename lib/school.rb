511a1a24d24d2acf7325d419bbd40d8210888f9f
Derick Quartey <niikwartey1@gmail.com>
2016-07-13 18:24:23 -0400

    finished school domain model lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+  attr_accessor :name
+  attr_reader :roster
+  
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+  
+   def add_student(student_name, grade)
+     if !@roster[grade]
+      @roster[grade] = []
+     end
+     @roster[grade] << student_name
+   end
+
+   def grade(grade)
+     @roster[grade]
+   end
+
+   def sort
+     @roster.each do |grade, students|
+      students.sort!
+     end
+   end
+end
