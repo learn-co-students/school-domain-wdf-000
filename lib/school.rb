44384ad5bc57e4fa324f7ef8162fc4135c6b8ebb
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-18 23:39:14 -0400

    updated school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,31 @@
 # code here!
 class School
-  def initialize(school)
-    @school = school
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
     @roster = {}
   end
 
-  def roster
-    @roster
-  end
+  # def roster
+  #   roster
+  # end
 
   def add_student(name, grade)
-    if @roster.length == 0 || !@roster.keys.include?(grade)
-      @roster[grade] = []
-      @roster[grade] << name
+    if roster.length == 0 || !@roster.keys.include?(grade)
+      roster[grade] = []
+      roster[grade] << name
     else
-      @roster[grade] << name
+      roster[grade] << name
     end
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
+    roster.each do |grade, name|
+      roster[grade] = name.sort
     end
   end
 
