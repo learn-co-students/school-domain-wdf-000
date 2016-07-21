aa3dce9b0abc5264b609a68f76e5989746abdcc7
parm530 <parmsahadeo530@gmail.com>
2016-07-21 11:48:29 -0400

    Edited file

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,29 @@
-require 'pry'
 # code here!
 class School
 
-  attr_reader :roster
+  attr_accessor :roster, :school_name
 
   def initialize (name_of_school)
+    @school_name = name_of_school
     @roster = {}
   end
 
   def add_student(name, grade)
+    if(roster.size == 0)
+      roster[grade] = []
+      roster[grade] << name
 
-    if(@roster.size == 0)
-      @roster[grade] = []
-      @roster[grade] << name
-
-    elsif(@roster[grade])
-      @roster[grade] << name
+    elsif(roster[grade])
+      roster[grade] << name
 
     else
-      @roster[grade] = []
-      @roster[grade] << name
-
+      roster[grade] = []
+      roster[grade] << name
     end
   end
 
   def grade(num)
-    @roster.each do |grade, student_array|
+    roster.each do |grade, student_array|
       if(num == grade)
         return student_array
       end
@@ -34,7 +32,7 @@ class School
 
   def sort
     temp_hash = {}
-    @roster.each do |grade, student_array|
+    roster.each do |grade, student_array|
       temp_hash[grade] = student_array.sort
     end
     temp_hash
