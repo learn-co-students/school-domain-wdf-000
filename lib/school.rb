dc2e0aed8ed93598ac4219a3f71cdac2a49bb16e
Keo  Bun <keonilrath@gmail.com>
2016-09-04 02:48:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  attr_reader :name, :roster
+  #attr_accessor :name, :roster
+
+  def initialize(school)
+    @name = school
+    @roster = {}
+  end
+
+  def add_student(names, grade)
+    @roster[grade] ||= []
+    @roster[grade] << names
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade, students| students.sort!}
+  end
+end
